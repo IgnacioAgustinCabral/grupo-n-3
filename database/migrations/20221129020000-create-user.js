1d893b4919d77beca2a6ec495d945f975fb9bfce
kkirvinnng <kevinreyna2480@gmail.com>
2022-11-29 11:40:32 -0300

    feat: add user model & migration

---
+++ database/migrations/20221129020000-create-user.js
@@ -0,0 +1,69 @@
+"use strict";
+const sequelize = require("sequelize");
+const models = require("../models");
+
+module.exports = {
+    up: async (queryInterface) => {
+        await queryInterface.sequelize.transaction(async (transaction) => {
+
+            return queryInterface.createTable(models.User.tableName, {
+                id: {
+                    allowNull: false,
+                    autoIncrement: true,
+                    primaryKey: true,
+                    type: sequelize.INTEGER,
+                },
+                firstName: {
+                    allowNull: false,
+                    type: sequelize.STRING,
+                },
+                lastName: {
+                    allowNull: false,
+                    type: sequelize.STRING,
+                },
+                email: {
+                    allowNull: false,
+                    unique: true,
+                    type: sequelize.STRING,
+                },
+                password: {
+                    allowNull: false,
+                    type: sequelize.STRING,
+                },
+                avatar: {
+                    allowNull: true,
+                    type: sequelize.STRING,
+                },
+                roleId: {
+                    allowNull: false,
+                    type: sequelize.INTEGER,
+                    references: {
+                        model: models.User.tableName,
+                        key: 'id'
+                    }
+                },
+                createdAt: {
+                    allowNull: false,
+                    type: sequelize.DATE,
+                },
+                updatedAt: {
+                    allowNull: false,
+                    type: sequelize.DATE,
+                },
+                deletedAt: {
+                    type: sequelize.DATE,
+                },
+            }, {
+
+                transaction,
+            });
+        });
+    },
+    down: async (queryInterface) => {
+        await queryInterface.sequelize.transaction(async (transaction) => {
+            return queryInterface.dropTable(models.User.tableName, {
+                transaction,
+            });
+        });
+    },
+};
