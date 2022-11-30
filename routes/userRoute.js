42660bfda043ba4a6053d0a0c2e6b2ea7f500af6
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2022-11-30 20:04:23 -0300

    added update PUT/:id endpoint

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { getAllUsers, getUser } = require('../controllers/userController');
+const { getAllUsers, getUser, updateUser} = require('../controllers/userController');
 
 const router = express.Router();
 
 router.get('/', getAllUsers);
 router.get('/:id', getUser);
+router.put('/:id', updateUser);
 
 module.exports = router;
