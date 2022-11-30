3176823705cb89855d4d8fd2181be495ea6614bf
Ignacio Agustin Cabral <ignaciocabral97@gmail.com>
2022-11-30 12:41:28 -0300

    Added User GET/:id endpoint

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getAllUsers } = require('../controllers/userController');
+const { getAllUsers, getUser } = require('../controllers/userController');
 
 const router = express.Router();
 
 router.get('/', getAllUsers);
+router.get('/:id', getUser);
 
 module.exports = router;
