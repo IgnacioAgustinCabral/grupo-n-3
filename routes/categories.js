61a518826dce345ac6f4dcebf52e28c17013e116
Emerson Villalta <reaperfocs@gmail.com>
2022-11-30 17:37:40 -0500

    Post and Update categories

--- routes/categories.js
+++ routes/categories.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const {
     allCategories,
-    getCategory
+    getCategory,
+    postCategory,
+    updateCategory
 } = require('../controllers/categoryController')
-
+const {validateRequestBySchema} = require('../middlewares/validateRequestSchema')
+const categorySchema = require('../schemas')
 const router = express.Router();
 
 router.get('', allCategories);
 router.get('/:id', getCategory);
-
+router.post('/',validateRequestBySchema(categorySchema), postCategory)
+router.patch('/:id', validateRequestBySchema(categorySchema), updateCategory)
 module.exports = router
