d7ee2df8281f523e478215a9bdd5dd2b09faf6d9
Alfredo Villegas <alfreddovillegas@gmail.com>
2022-11-30 19:59:41 -0400

    Feature/transaction post service (#14)
    * feat: added create transaction service and controller
    
    * feat: added request validation in post /Transaction

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const { getAllTransactions, postTransaction } = require('../controllers/transactionControllers');
+const { validateRequestBySchema } = require('../middlewares/validateRequestSchema');
+const { postTransactionSchema } = require('../schemas/transactionSchemas');
 
 const router = express.Router();
 
 router.get('/', getAllTransactions);
+router.post('/', validateRequestBySchema(postTransactionSchema), postTransaction)
 
 module.exports = router;
--- services/transactionServices.js
+++ services/transactionServices.js
@@ -4,6 +4,11 @@ async function findAllTransaction() {
   return await Transaction.findAll();
 }
 
+async function createTransaction(newTransaction) {
+  return await Transaction.create(newTransaction);
+}
+
 module.exports = {
   findAllTransaction,
+  createTransaction
 };
