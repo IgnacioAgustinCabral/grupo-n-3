eea6359cad4740527d7e32c591e13686f679c79b
Alfredo Villegas <alfreddovillegas@gmail.com>
2022-11-30 15:33:36 -0400

    feat: added request validation in post /Transaction

--- routes/transactionRoute.js
+++ routes/transactionRoute.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { getAllTransactions, postTransaction } = require('../controllers/transactionControllers');
+const { validateRequestBySchema } = require('../middlewares/validateRequestSchema');
+const { postTransactionSchema } = require('../schemas/transactionSchemas');
 
 const router = express.Router();
 
 router.get('/', getAllTransactions);
-router.post('/', postTransaction)
+router.post('/', validateRequestBySchema(postTransactionSchema), postTransaction)
 
 module.exports = router;
