c28840b2ff959cd68ad3289c137d00e0b87f5d6d
Ignacio Agustin Cabral <56377164+IgnacioAgustinCabral@users.noreply.github.com>
2022-12-01 19:05:48 -0300

    added update PUT/:id endpoint (#16)
    Merge conflict resolved

--- controllers/userController.js
+++ controllers/userController.js
@@ -62,4 +62,29 @@ module.exports = {
       next(httpError);
     }
   }),
+  updateUser: catchAsync(async (req, res, next) => {
+		const { id } = req.params;
+		const  body  = req.body;
+		try {
+			const user = await User.findByPk(id);
+			if (!user) {
+				const httpError = createHttpError('user not found', 404);
+				return next(httpError);
+			}
+			await User.update(body, { where: { id } });
+            //find updated user
+            const updatedUser = await User.findByPk(id);
+			endpointResponse({
+				res,
+				message: 'User updated successfully',
+				body: updatedUser,
+			});
+		} catch (error) {
+			const httpError = createHttpError(
+				error.statusCode,
+				`[Error updating User] - [index - PUT]: ${error.message}`
+			);
+			next(httpError);
+		}
+	}),
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -3,6 +3,7 @@ const {
   getAllUsers,
   getUser,
   createUser,
+  updateUser
 } = require("../controllers/userController");
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/", getAllUsers);
 router.get("/:id", getUser);
 router.post("/create", createUser);
+router.put('/:id', updateUser);
 
 module.exports = router;
