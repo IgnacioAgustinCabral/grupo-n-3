dfdb8326ff1b15faa45169db4b6c1fe4b0d0ec95
Brunoo9 <brunitoo903@gmail.com>
2022-12-09 20:45:19 -0300

    feat/pagination-users

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,18 +2,28 @@ const createHttpError = require("http-errors");
 const { endpointResponse } = require("../helpers/success");
 const { catchAsync } = require("../helpers/catchAsync");
 const { User } = require("../database/models");
-const { findOrCreateUser, deleteUser } = require("../services/userServices");
+const {
+  findAllUsers,
+  findOrCreateUser,
+  deleteUser,
+} = require("../services/userServices");
 
 module.exports = {
   getAllUsers: catchAsync(async (req, res, next) => {
+    let { page = 0 } = req.query;
     try {
-      const users = await User.findAll({
-        attributes: ["firstName", "lastName", "email", "createdAt"],
-      });
+      const { count, rows, prevPage, nextPage } = await findAllUsers(
+        Number(page)
+      );
       endpointResponse({
         res,
         message: "Users retrieved successfully",
-        body: users,
+        body: {
+          total: count,
+          prevPage,
+          nextPage,
+          users: rows,
+        },
       });
     } catch (error) {
       const httpError = createHttpError(
--- services/userServices.js
+++ services/userServices.js
@@ -2,6 +2,29 @@ const { User } = require("../database/models");
 const { genSaltSync, hashSync } = require("bcrypt");
 const { ErrorObject } = require("../helpers/error");
 
+async function findAllUsers(page) {
+  let pageUrl = "http://localhost:3000/users?page=",
+    prevPage = null,
+    nextPage = null,
+    limit = 10,
+    offset = page * limit, //ex: page 0 * limit 10 = 0 - show since register 0-
+    pages = 0; // quantity of pages
+
+  const { count, rows } = await User.findAndCountAll({
+    attributes: ["firstName", "lastName", "email", "createdAt"],
+    limit,
+    offset,
+  });
+  // ex: 12 registers / 10 limit = 2 pages in total - 1 because page initial value is 0
+  pages = Math.ceil(count / limit) - 1;
+
+  // actual page greater than 0 and actual page is less than  quantities of pages
+  if (page > 0 && page <= pages) prevPage = `${pageUrl}${page - 1}`;
+
+  // actual page is less than quantities of pages
+  if (page < pages) nextPage = `${pageUrl}${page + 1}`;
+  return { count, rows, prevPage, nextPage };
+}
 async function findOrCreateUser(
   { firstName, lastName, email, roleId, password },
   avatar
@@ -33,6 +56,7 @@ async function deleteUser(id) {
   return userDeleted;
 }
 module.exports = {
+  findAllUsers,
   findOrCreateUser,
   deleteUser,
 };
