57a36e44d452f97baed523fe005a4fa54543929f
Alfredo Villegas <alfreddovillegas@gmail.com>
2022-12-13 13:00:30 -0400

    feat: add middelwares for varify Admin role

---
+++ middlewares/isAdminRole.js
@@ -0,0 +1,23 @@
+const createHttpError = require("http-errors");
+
+const httpUnauthorizedError = createHttpError(
+  403,
+  `[Error] - [Unauthorized]: ${"Access denied"}`,
+);
+
+const isAdminRole = (req, res, next) => {
+  try {
+    const user = req.user;
+
+    const userIsAdmin = user.roleId === 1;
+    if (!userIsAdmin) next(httpUnauthorizedError);
+
+    return next();
+  } catch (error) {
+    next(httpUnauthorizedError);
+  }
+};
+
+module.exports = {
+  isAdminRole,
+};
