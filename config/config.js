9e9e88a9f40c4ff948c9c12e9292df161328a8a1
Alfredo Villegas <alfreddovillegas@gmail.com>
2022-12-14 01:55:03 -0400

    Config: add config for jest environment

--- config/config.js
+++ config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 
 module.exports = {
   development: {
@@ -7,7 +7,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'mysql',
+    dialect: "mysql",
   },
 
   production: {
@@ -16,6 +16,15 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'mysql',
+    dialect: "mysql",
   },
-}
+
+  test: {
+    username: process.env.DB_USER_TEST,
+    password: process.env.DB_PASSWORD_TEST,
+    database: process.env.DB_NAME_TEST,
+    host: process.env.DB_HOST_TEST,
+    port: process.env.DB_PORT_TEST,
+    dialect: "mysql",
+  },
+};
