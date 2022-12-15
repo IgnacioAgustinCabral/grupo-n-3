de9b4f330b9b6e2bf4be90dea14f5b3d8b7330c3
Alfredo Villegas <alfreddovillegas@gmail.com>
2022-12-14 22:09:52 -0400

    Feature/transaction tests (#45)
    * Config: add config for jest environment
    
    * feat: add post transaction test
    
    * test: add  post, put, delete transaction test
    
    * refactor: allow decimal in transaction Model
    
    * refactor: reuse the find transactions all service
    
    * feat: add tests for GET all transaction

--- middlewares/isOwnerTransaction.js
+++ middlewares/isOwnerTransaction.js
@@ -1,25 +1,30 @@
+const createHttpError = require("http-errors");
+const { ErrorObject } = require("../helpers/error");
 const { findOneTransaction } = require("../services/transactionServices");
 const { httpUnauthorizedError } = require("./isAdminRole");
 
 const isOwnerTransaction = async (req, res, next) => {
-    try {
-        const tansactionIdParams = parseInt(req.params.id)
+  try {
+    const tansactionIdParams = parseInt(req.params.id);
 
-        const userAuthenticated = req.user
-        const transaction = await findOneTransaction(tansactionIdParams)
+    const userAuthenticated = req.user;
+    const transaction = await findOneTransaction(tansactionIdParams);
 
-        const isOwner = userAuthenticated.id === transaction.userId;
-        const isAdmin = userAuthenticated.roleId === 1;
-        if (isOwner || isAdmin) {
-            return next();
-        }
-
-        next(httpUnauthorizedError)
-    } catch (e) {
-        next(httpUnauthorizedError)
+    const isOwner = userAuthenticated.id === transaction.userId;
+    const isAdmin = userAuthenticated.roleId === 1;
+    if (isOwner || isAdmin) {
+      return next();
     }
-}
+
+    next(httpUnauthorizedError);
+  } catch (e) {
+    if (e instanceof ErrorObject) {
+      return next(createHttpError(e.statusCode, `[Error] : ${e.message}`));
+    }
+    next(httpUnauthorizedError);
+  }
+};
 
 module.exports = {
-    isOwnerTransaction
-}
+  isOwnerTransaction,
+};
